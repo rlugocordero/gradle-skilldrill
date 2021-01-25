27b7310558bebc9842348b49a5edf80a1c299768
Roberto Lugo <rlugocordero@gmail.com>
2021-01-24 20:42:41 -0700

    What is going on

--- app/src/main/java/edu/isu/cs2263/Student.java
+++ app/src/main/java/edu/isu/cs2263/Student.java
@@ -1,10 +1,18 @@
+package edu.isu.cs2263;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.awt.*;
 
 public class Student extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("First JavaFX Application");
+        stage.setTitle("Course View");
+
+        Label label = new Label("Hello World");
+        Scene scene = new Scene(label,400,200);
+
         stage.show();
 
     }
